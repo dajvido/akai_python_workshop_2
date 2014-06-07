4684d45bae14faecc2c7c827dfc8392dd8f74d6d
dajvido <dawid.jaskot@gmail.com>
2014-06-07 18:01:00 +0200

    Done exercises and tests

--- calc/calc/calc.py
+++ calc/calc/calc.py
@@ -1,31 +1,42 @@
-import sys
 import random
 import logging
 import re
+import sys
 
 logging.getLogger().setLevel(logging.DEBUG)
+
 
 def add(x, y):
     return x + y
 
+
 def sub(x, y):
     return x - y
+
 
 def mul(x, y):
     return x * y
 
-def div(x, y):
-    return x // y
 
-#TODO: 2. adding mul and div operators
-#TODO: 3. add power function, handle operator priority
-#TODO: 4. suppring float
+def div(x, y):
+    return x / y
+
+
+def power(x, y):
+    return x ** y
+
+
 OPERATORS = {
     "+": add,
     "-": sub,
     "*": mul,
     "/": div,
+    "^": power,
 }
+OPERATORS_AT_THE_BEGINNING = ['add', 'sub']
+HIGH_PRIORITY_OPERATORS = ['power']
+MEDIUM_PRIORITY_OPERATORS = ['mul', 'div']
+
 
 def eval_var(var_name):
     if var_name == 'R':
@@ -33,61 +44,112 @@ def eval_var(var_name):
     elif var_name == 'G':
         return 9.81
     else:
-        return ValueError('Variable %s not supported' % var_name)
+        raise ValueError('Variable %s not supported' % var_name)
+
 
 def validate_eq(parsed_input):
     """Checks if equation is correct
     3+-5+RR -> ValueError
     """
-    #TODO: 1. check if every 2nd value is an operator
-    for i in range(len(parsed_input)):
-        if i % 2 == 1:
-            if parsed_input[i] not in OPERATORS.values():
-                raise ValueError('Even token is not an operator')
-        elif parsed_input[i] in OPERATORS.values():
-            raise ValueError('Odd token is an operator')
+    if not isinstance(parsed_input[0], float) \
+       and not isinstance(parsed_input[0], str):
+
+        if parsed_input[0].__name__ not in OPERATORS_AT_THE_BEGINNING:
+            raise ValueError("Invalid operator %s at the beginning"
+                             % parsed_input[0])
+
+    elif not isinstance(parsed_input[len(parsed_input) - 1], float) \
+            and not isinstance(parsed_input[len(parsed_input) - 1], str):
+
+        raise ValueError("Invalid operator %s at the end"
+                         % parsed_input[len(parsed_input) - 1])
+
+    else:
+        for i in range(1, len(parsed_input) - 1):
+            if not isinstance(parsed_input[i], float) \
+               and not isinstance(parsed_input[i], str):
+                if (not isinstance(parsed_input[i - 1], float)
+                    and not isinstance(parsed_input[i - 1], str)) \
+                   or (not isinstance(parsed_input[i + 1], float)
+                       and not isinstance(parsed_input[i + 1], str)):
+
+                    raise ValueError("Invalid combination %s %s %s"
+                                     % (parsed_input[i - 1], parsed_input[i],
+                                        parsed_input[i + 1]))
 
 
 def parse_input(raw_input):
     """returns a array of eq elements
-    2+5-R+3 -> [2, add, 5, sub, 'R', add, 3]
+    2+5-R+3 -> ['2', '+', '5', '-', 'R', '+', '3']
     """
-    split_input = re.split("([\+-])", raw_input.replace(' ', ''))
-    if split_input:
+
+    split_input = re.split("([\*^/\+-])", raw_input.replace(' ', ''))
+    if isinstance(split_input, list) and split_input != ['']:
+        split_input = [item for item in split_input if item != '']
         for i in range(len(split_input)):
             if split_input[i] in OPERATORS:
-                # replace operator, with eval fn
                 split_input[i] = OPERATORS[split_input[i]]
-            elif split_input[i].isdigit():
-                split_input[i] = int(split_input[i])
+            elif split_input[i].replace(".", "").isdigit():
+                split_input[i] = float(split_input[i])
         return split_input
     raise ValueError("Invalid Input data %s" % raw_input)
 
+
 def solve_eq(parsed_input):
     stack = []
+    if not isinstance(parsed_input[0], float) \
+       and not isinstance(parsed_input[0], str):
+
+        operator = parsed_input[0]
+        del parsed_input[0]
+        parsed_input[0] = operator(0.0, parsed_input[0])
+
     for el in parsed_input:
         if isinstance(el, str):
-            el = eval_var(el)
+            el = float(eval_var(el))
         stack.append(el)
-        if len(stack) >= 3:
-            val_1, operator, val_2 = stack[0:3]
-            stack[0] = operator(val_1, val_2)
-            del stack[1:]
+    for i in range(len(parsed_input) - 1, 1, -1):
+        if not isinstance(parsed_input[i], float) \
+           and not isinstance(parsed_input[i], str):
 
-    assert len(stack) == 1
-    return stack[0]
+            if parsed_input[i].__name__ in HIGH_PRIORITY_OPERATORS:
+                val_1, operator, val_2 = stack[(i - 1):(i + 2)]
+                stack[i - 1] = operator(val_1, val_2)
+                del stack[i:(i + 2)]
+    stack_len = len(stack)
+    stack.reverse()
+    for i in range(stack_len - 2, 0, -1):
+        if not isinstance(stack[i], float) \
+           and not isinstance(stack[i], str):
+            if stack[i].__name__ in MEDIUM_PRIORITY_OPERATORS:
+                val_2, operator, val_1 = stack[(i - 1):(i + 2)]
+                stack[i - 1] = operator(val_1, val_2)
+                del stack[i:(i + 2)]
+    stack.reverse()
+    new_stack = []
+    for el in stack:
+        new_stack.append(el)
+        if len(new_stack) >= 3:
+            val_1, operator, val_2 = new_stack[0:3]
+            new_stack[0] = operator(val_1, val_2)
+            del new_stack[1:]
+    assert len(new_stack) == 1
+    return new_stack[0]
+
 
 def calculate(raw_input):
-    """Solves equation passed as raw_input, returns int value
+    """Solves equation passed as raw_input, returns float value
     """
     parsed_input = parse_input(raw_input)
-
     validate_eq(parsed_input)
     return solve_eq(parsed_input)
 
+
 def main():
+    """Main function
+    """
     eq = sys.argv[1]
     print(calculate(eq))
 
 if __name__ == "__main__":
-    print(calculate("1+2-5+R"))
+    print(calculate("1.3+2*3-2^3-5/2+R"))
--- calc/calc/tests/test_calc.py
+++ calc/calc/tests/test_calc.py
@@ -1,29 +1,78 @@
 import pytest
 import unittest.mock
-import random
-
 
 from calc import (
+    main,
     calculate,
-    validate_eq,
-    add,
 )
 
-def foo(times=3):
-    return "1+" * times + "3"
+
+def test_simple_add_eq():
+    assert(calculate('1+2') == 3.0)
 
 
-def test_simple_eq():
-    tmp = calculate(foo())
-    print(tmp, "really?")
-    assert(tmp == 7)
-    assert(calculate(foo()) == 6)
+def test_simple_sub_eq():
+    assert(calculate('3-2') == 1.0)
 
-def test_variables():
+
+def test_simple_mul_eq():
+    assert(calculate('2*2') == 4.0)
+
+
+def test_simple_div_eq():
+    assert(calculate('1/2') == 0.5)
+
+
+def test_simple_power_eq():
+    assert(calculate('2^3') == 8.0)
+
+
+def test_complicated_eq():
     with unittest.mock.patch("random.randint", lambda x, y: 0):
-        assert(calculate("R+0") == 0)
+        assert(calculate("1.3+2*3-2^3-5/2+R") == -3.2)
+
+
+def test_supported_variables():
+    with unittest.mock.patch("random.randint", lambda x, y: 0):
+        assert(calculate("R+G") == 9.81)
+
+
+def test_not_supported_variable():
+    with pytest.raises(ValueError):
+        calculate("Z+5")
 
 
 def test_validate_eq():
     with pytest.raises(ValueError):
-        validate_eq([5, add, "r", add, add])
+        calculate('3+-5+RR')
+
+
+def test_valid_operator_at_the_beginning():
+    assert(calculate("-5+3") == -2.0)
+
+
+def test_invalid_operator_at_the_beginning():
+    with pytest.raises(ValueError):
+        calculate("*5+3")
+
+
+def test_invalid_operator_at_the_end():
+    with pytest.raises(ValueError):
+        calculate("5+3+")
+
+
+def test_invalid_combination():
+    with pytest.raises(ValueError):
+        calculate("5+-3")
+
+
+def test_invalid_input_data():
+    with pytest.raises(ValueError):
+        calculate("")
+
+
+def test_run_with_arguments(capfd):
+    with unittest.mock.patch("sys.argv", ["calc.py", "1+3"]):
+        main()
+        out, _ = capfd.readouterr()
+        assert out.strip() == "4.0"
